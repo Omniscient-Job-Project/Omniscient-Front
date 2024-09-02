cdf23e914b3e2dab383c40bb327a8d014ad8c479
KHY90 <power4206@gmail.com>
2024-09-03 01:02:30 +0900

    fix:index

--- src/components/index.vue
+++ src/components/index.vue
@@ -13,7 +13,7 @@
         <div class="card">
           <img src="..." class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">채용 제목</h5>
             <p class="card-text">
               This is a wider card with supporting text below as a natural lead-in
               to additional content. This content is a little bit longer.
