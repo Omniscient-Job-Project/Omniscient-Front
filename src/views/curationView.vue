4ded3538812a683334dbcedf6afcca284e9f9dca
KHY90 <power4206@gmail.com>
2024-09-02 18:12:50 +0900

    feat:curation

---
+++ src/views/curationView.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header />
+    <!-- <CurationSelect /> -->
+    <CurationMain />
+    <Footer />
+</template>
+
+<script setup>
+import Header from '@/components/header/header.vue'; // 헤더 컴포넌트 임포트
+import Footer from '@/components/footer/footer.vue'; // 풋터 불러오기
+// import CurationSelect from '@/components/curation/curationSelect.vue';
+import CurationMain from '@/components/curation/curationMain.vue';
+</script>
+
+<style>
+    
+</style>
