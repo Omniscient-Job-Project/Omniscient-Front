f95d54e869933c1d6361832913c7cf37669ee057
KHY90 <power4206@gmail.com>
2024-09-03 12:14:56 +0900

    feat:login

---
+++ src/views/loginView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from '@/components/header/header.vue'; // 헤더 컴포넌트 임포트
+import Login from '@/components/login/login.vue';
+import Footer from '@/components/footer/footer.vue'; // 풋터 불러오기
+</script>
+
+<template>
+    <Header />
+    <Login />
+    <Footer />
+</template>
+
+<style>
+</style>
