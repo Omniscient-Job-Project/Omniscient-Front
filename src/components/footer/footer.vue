64326b50e3b8f6fc05da196d17e1bfa88306fce4
joo <zhuyaan93@gmail.com>
2024-09-05 16:59:44 +0900

    fix:footer

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -1,21 +1,18 @@
 <template>
-
   <footer>
-      <div class="footer">
-        <RouterLink to="/notice" style="text-decoration: none;">공지사항</RouterLink>
-        <div>이용약관</div>
-        <div>자주 묻는 질문(FAQ)</div>
-        <div><a href="https://github.com/Omniscient-Job-Project" target="_blank" style="text-decoration: none;">깃허브</a>
-        </div>
-        <div>전직시</div>
+    <div class="footer">
+      <RouterLink to="/notice" class="footer-link">공지사항</RouterLink>
+      <div class="footer-link">이용약관</div>
+      <div class="footer-link">자주 묻는 질문(FAQ)</div>
+      <div class="footer-link">
+        <a href="https://github.com/Omniscient-Job-Project" target="_blank" class="footer-link">깃허브</a>
       </div>
-    </footer>
-  
-    </template>
-    <script setup>
-    import { RouterLink } from 'vue-router';
-     import '@/assets/css/footer.css';
-   </script>
-  
-   
-  
+      <div class="footer-link">전직시</div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+import '@/assets/css/footer.css';
+</script>
