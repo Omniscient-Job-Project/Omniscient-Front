b5214514ed833ba02d6fb93fa180ff5380e6f8e6
wsy <wsy9088@gmail.com>
2024-09-16 01:48:38 +0900

    feat termsView

---
+++ src/views/termsView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from '@/components/header/header.vue';
+import Footer from '@/components/footer/footer.vue';
+import Terms from '@/components/terms/terms.vue';
+</script>
+
+<template>
+    <Header/>
+        <Terms />
+    <Footer/>
+</template>
+
+<style>
+</style>
